3b68229454e3fee88e91c07ac799f3761398c5e5
Chetan2028 <bmchetan2028@gmail.com>
2020-10-25 21:25:39 +0530

    [Chetan] Add . Added to calculate part time employee wage

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,7 +5,8 @@ namespace EmployeeWageComputation
     class Program
     {
         ///Constants
-        const int IS_PRESENT = 1;
+        const int IS_FULL_TIME = 2;
+        const int IS_PART_TIME = 1;
         const int EMPLOYEE_WAGE_PER_HOUR = 20;
 
 
@@ -19,13 +20,20 @@ namespace EmployeeWageComputation
         {
             int empCheck = random.Next(2);
             int employeeHours;
-            if (empCheck == IS_PRESENT)
+            if (empCheck == IS_FULL_TIME)
             {
                 employeeHours = 8;
+                Console.WriteLine("Employee is Full Time");
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                employeeHours = 4;
+                Console.WriteLine("Employee is Part Time");
             }
             else
             {
                 employeeHours = 0;
+                Console.WriteLine("Employee is Absent");
             }
             Console.WriteLine("Employee Daily Wage is : " + employeeHours * EMPLOYEE_WAGE_PER_HOUR);
         }
