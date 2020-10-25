3356ce624917bfaf82316b06b3dfd4e8f472602a
Chetan2028 <bmchetan2028@gmail.com>
2020-10-25 21:21:42 +0530

    Revert "[Chetan] Add . Added to calculate part time employee wage"
    This reverts commit c80ff8a02dbc01f2f4a2e5511d4ac0ebd84540a2.

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,8 +5,7 @@ namespace EmployeeWageComputation
     class Program
     {
         ///Constants
-        const int IS_FULL_TIME = 2;
-        const int IS_PART_TIME = 1;
+        const int IS_PRESENT = 1;
         const int EMPLOYEE_WAGE_PER_HOUR = 20;
 
 
@@ -16,24 +15,17 @@ namespace EmployeeWageComputation
         /// <summary>
         /// Calculates the daily wage for part time employee.
         /// </summary>
-        public void CalculateDailyWageForPartTimeEmployee()
+        public void CalculateDailyWage()
         {
             int empCheck = random.Next(2);
             int employeeHours;
-            if (empCheck == IS_FULL_TIME)
+            if (empCheck == IS_PRESENT)
             {
                 employeeHours = 8;
-                Console.WriteLine("Employee is Full Time");
-            }
-            else if(empCheck == IS_PART_TIME)
-            {
-                employeeHours = 4;
-                Console.WriteLine("Employee is Part Time");
             }
             else
             {
                 employeeHours = 0;
-                Console.WriteLine("Employee is Absent");
             }
             Console.WriteLine("Employee Daily Wage is : " + employeeHours * EMPLOYEE_WAGE_PER_HOUR);
         }
@@ -44,7 +36,7 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             Program program = new Program();
-            program.CalculateDailyWageForPartTimeEmployee();
+            program.CalculateDailyWage();
         }
     }
 }
