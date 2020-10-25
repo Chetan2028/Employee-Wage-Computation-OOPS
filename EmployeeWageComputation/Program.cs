cc9a29ed76ebfa2a35b30620e3ceceba62cc79af
Chetan2028 <bmchetan2028@gmail.com>
2020-10-25 21:00:01 +0530

    [Chetan] Add . Added Program file to check for employee

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -4,9 +4,33 @@ namespace EmployeeWageComputation
 {
     class Program
     {
+        ///Variables
+        const int IS_PRESENT = 1;
+
+        /// <summary>
+        /// Checks for employee presence.
+        /// </summary>
+        public void CheckForEmployeePresence()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(2);
+            if (empCheck == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program program = new Program();
+            program.CheckForEmployeePresence();
         }
     }
 }
